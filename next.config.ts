cbfde8bc0b1d6a51b24dfa4da633365f32f9b2b7
SukaMCD <fabian25march@gmail.com>
2026-06-01 00:30:40 +0700

    feat: add rewrite rule to map /mcdwallet to /projects/mcdwallet

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.100.6'],
+  async rewrites() {
+    return [
+      {
+        source: '/mcdwallet',
+        destination: '/projects/mcdwallet',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
